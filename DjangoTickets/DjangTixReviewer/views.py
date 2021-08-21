42d5d023086fb0fb45682d3a0f582598461568ee
Daniel Wile <danielhwile@gmail.com>
2021-08-20 21:00:30 -0400

    Added App and Wired Initial Home URLs

--- DjangoTickets/DjangTixReviewer/views.py
+++ DjangoTickets/DjangTixReviewer/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
 # Create your views here.
+def home(request):
+    context = {"name": "Daniel"}
+    return render(request, "DjangTixReviewer/home.html", context)
